3cfd8875592eea569aad632d7f1c94d4e16b862f
Siddhartha Barman Joy <siddharthjoy88@gmail.com>
2022-05-06 18:38:27 +0600

    Update darethedragon.sol

--- darethedragon.sol
+++ darethedragon.sol
@@ -9,7 +9,6 @@ contract Access_Control_Protocol{
     uint64 public idx = 0;
     string public state = "off";
     string public got;
-    // uint64 public toggle = 1;
 
     address admin;
     mapping (address => uint64) public balanceOf;
@@ -25,7 +24,6 @@ contract Access_Control_Protocol{
 
   
       function _transfer(address _from, address _to, uint64 _value) internal {
-        // require(_to != 0x0);
         require(balanceOf[_from] >= _value);
         require(balanceOf[_to] + _value > balanceOf[_to]);
         uint previousBalances = balanceOf[_from] + balanceOf[_to];
@@ -34,85 +32,81 @@ contract Access_Control_Protocol{
         emit Transfer(_from, _to, _value);
         assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
-    
-// }
 
-// contract acces_control is AccessControl_digitalSig
-// {
  
-uint64 public time_start; 
-uint64 public time_end; 
-uint64 _uid = 1;
+    uint64 public time_start; 
+    uint64 public time_end; 
+    uint64 _uid = 1;
 
-string location;
-mapping(address=>role) white_list;
-address public ad_device_owner;
+    string location;
+    mapping(address=>role) white_list;
+    address public ad_device_owner;
 
-enum role{
-    Ressource_Owner,NetworkEngineer,Service_Provider,Blaclisted
-}
-
-function getMessageHash(string memory _message,uint _nonce) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_message, _nonce));
-}
-
-
-function getEthSignedMessageHash(bytes32 _messageHash) public pure returns (bytes32){
-        return
-            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash));
-}
-
-
-function verify(address _signer,string memory _message,uint _nonce,bytes memory signature) public pure returns (bool) {
-        bytes32 messageHash = getMessageHash(_message, _nonce);
-        bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
-
-        return recoverSigner(ethSignedMessageHash, signature) == _signer;
-}
-
-function recoverSigner(bytes32 _ethSignedMessageHash, bytes memory _signature) public pure returns (address){
-        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
-
-        return ecrecover(_ethSignedMessageHash, v, r, s);
-}
-
-function splitSignature(bytes memory sig) public pure returns (bytes32 r,bytes32 s,uint8 v){
-        require(sig.length == 65, "invalid signature length");
-
-        assembly {
-            r := mload(add(sig, 32))
-            s := mload(add(sig, 64))
-            v := byte(0, mload(add(sig, 96)))
-        }
+    enum role{
+        Ressource_Owner,NetworkEngineer,Service_Provider,Blaclisted
     }
 
-function Acces_control(string memory device_location,uint64 access_time_start,uint64 access_time_end,uint64 initialSupply) public { 
-        totalSupply = uint64(initialSupply * 10 ** uint64(decimals)); 
-        ad_device_owner=msg.sender;
-        admin=ad_device_owner;
-        white_list[ad_device_owner]=role.Ressource_Owner; 
-        time_start=access_time_start;
-        time_end=access_time_end;
-    
-        location=device_location;
-        balanceOf[ad_device_owner] = totalSupply;
-       
-}   
+    function getMessageHash(string memory _message,uint _nonce) public pure returns (bytes32) {
+            return keccak256(abi.encodePacked(_message, _nonce));
+    }
 
 
-function getViolationHistory()public view returns( address  [] memory){
-    return violation;
-}
+    function getEthSignedMessageHash(bytes32 _messageHash) public pure returns (bytes32){
+            return
+                keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash));
+    }
 
 
-   modifier only_ressource_owner() 
+    function verify(address _signer,string memory _message,uint _nonce,bytes memory signature) public pure returns (bool) {
+            bytes32 messageHash = getMessageHash(_message, _nonce);
+            bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
+
+            return recoverSigner(ethSignedMessageHash, signature) == _signer;
+    }
+
+    function recoverSigner(bytes32 _ethSignedMessageHash, bytes memory _signature) public pure returns (address){
+            (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+
+            return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
+
+    function splitSignature(bytes memory sig) public pure returns (bytes32 r,bytes32 s,uint8 v){
+            require(sig.length == 65, "invalid signature length");
+
+            assembly {
+                r := mload(add(sig, 32))
+                s := mload(add(sig, 64))
+                v := byte(0, mload(add(sig, 96)))
+            }
+        }
+
+    function Acces_control(string memory device_location,uint64 access_time_start,uint64 access_time_end,uint64 initialSupply) public { 
+            totalSupply = uint64(initialSupply * 10 ** uint64(decimals)); 
+            ad_device_owner=msg.sender;
+            admin=ad_device_owner;
+            white_list[ad_device_owner]=role.Ressource_Owner; 
+            time_start=access_time_start;
+            time_end=access_time_end;
+
+            location=device_location;
+            balanceOf[ad_device_owner] = totalSupply;
+
+    }   
+
+
+    function getViolationHistory()public view returns( address  [] memory){
+        return violation;
+    }
+
+
+    modifier only_ressource_owner() 
     {
         require(white_list[msg.sender]==role.Ressource_Owner);
         _;
     }
 
 
-event allow_access_event(bool allowed);
+    event allow_access_event(bool allowed);
 
     function setRole (address ad, uint64  roleID) public only_ressource_owner 
     {
@@ -125,10 +119,10 @@ event allow_access_event(bool allowed);
             white_list[ad]=role.Service_Provider; 
         if (roleID==3)
             white_list[ad]=role.Blaclisted; 
-        
+
     }
 
- 
+
     function getrole(address ad) public only_ressource_owner view returns (string memory) 
     {
         if(white_list[ad] == role.Ressource_Owner) return "RO";
@@ -137,7 +131,7 @@ event allow_access_event(bool allowed);
         if(white_list[ad] == role.Blaclisted) return "Blacklisted";
         //return ;
     }
-    
+
     function get_token_balance(address ad) public only_ressource_owner view returns(uint64)
     {
         return balanceOf[ad];
@@ -149,10 +143,10 @@ event allow_access_event(bool allowed);
     }
 
 
-function access_control_policy(address requester, string memory location_,bytes memory signature)public returns (string memory) { 
-    
+    function access_control_policy(address requester, string memory location_,bytes memory signature)public returns (string memory) { 
+
     string memory loc = "serverroom";
-    
+
     if(keccak256(abi.encodePacked((location_))) == keccak256(abi.encodePacked((loc))))
     {
                 bytes32 messageHash = getMessageHash(location_, _uid); 
@@ -185,9 +179,9 @@ function access_control_policy(address requester, string memory location_,bytes 
         }
         return "Not Permitted";
     }
-}
+    }
 
-function Access_Request(string memory ressource_, string memory location,bytes memory signature) public returns (string memory){ 
+    function Access_Request(string memory ressource_, string memory location,bytes memory signature) public returns (string memory){ 
      string memory res = "raspberry";
     if(keccak256(abi.encodePacked((ressource_))) == keccak256(abi.encodePacked((res))) && white_list[msg.sender] == role.NetworkEngineer || white_list[msg.sender]==role.Ressource_Owner || white_list[msg.sender]==role.Service_Provider)
     {
@@ -200,38 +194,36 @@ function Access_Request(string memory ressource_, string memory location,bytes m
         // return false;
         //revert();
      }
-}
+    }
 
-function token_back(uint64 amount) public{
-    _transfer(msg.sender,ad_device_owner,amount);
-}
+    function token_back(uint64 amount) public{
+        _transfer(msg.sender,ad_device_owner,amount);
+    }
 
-function toggle(string memory ressource_, string memory location,bytes memory signature) public returns (string memory){
-    require(white_list[msg.sender]==role.Service_Provider);
-    string memory result = Access_Request(ressource_,location,signature);
-    if(keccak256(abi.encodePacked((result))) == keccak256(abi.encodePacked(("Not Permitted")))) return result;
-    else{
-        if(keccak256(abi.encodePacked((result))) == keccak256(abi.encodePacked(("on")))){
-            state = "off";
-            return "LED was ON,Toggled to OFF";
-        }
-        else if(keccak256(abi.encodePacked((result))) == keccak256(abi.encodePacked(("off")))){
-            state = "on";
-            return "LED was OFF,Toggled to ON";
-        }
+    function toggle(string memory ressource_, string memory location,bytes memory signature) public returns (string memory){
+        require(white_list[msg.sender]==role.Service_Provider);
+        string memory result = Access_Request(ressource_,location,signature);
+        if(keccak256(abi.encodePacked((result))) == keccak256(abi.encodePacked(("Not Permitted")))) return result;
         else{
-            return "LED state is NULL";
-        }
-    } 
-}
+            if(keccak256(abi.encodePacked((result))) == keccak256(abi.encodePacked(("on")))){
+                state = "off";
+                return "LED was ON,Toggled to OFF";
+            }
+            else if(keccak256(abi.encodePacked((result))) == keccak256(abi.encodePacked(("off")))){
+                state = "on";
+                return "LED was OFF,Toggled to ON";
+            }
+            else{
+                return "LED state is NULL";
+            }
+        } 
+    }
 
 }
 
 
 
 // ethereum.request({ method: "personal_sign", params: [account, hash]}).then(console.log)
-
-// 0x77611D748d44fa83ae85348E0497CDC7699d014b
 
 //  0xc6145f8562fe132a277c592b7eaeff400ffe2fe2140acf84a678013f3f2bea6b
 
